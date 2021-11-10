11a8c63d3d4b36ddaa1faf4811eeed189010747d
Juanjo128 <53496444+Juanjo128@users.noreply.github.com>
2021-11-09 22:20:55 -0500

    Update Asignatura.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -9,12 +9,8 @@ public class Asignatura {
 
     Asignatura() {
         this("Sin nombre");
-        this(0);
     }
 
-    Asignatura(int codigoInterno) {
-        this("Sin nombre", codigoInterno, 0);
-    }
 
     Asignatura(int codigoExterno) {
         this("Sin nombre", 0, codigoExterno);
@@ -36,9 +32,6 @@ public class Asignatura {
         this.nombre = nombre;
     }
 
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
 
     void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
