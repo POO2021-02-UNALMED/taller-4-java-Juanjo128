09ddfd6fd4259967dcd3b72af2cd1f256190ea98
Juanjo128 <53496444+Juanjo128@users.noreply.github.com>
2021-11-09 22:21:07 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,15 +2,17 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+     long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
-
+    
+    Persona(){
+    	this.nombre = "Sin nombre";
+    }
     Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -42,7 +44,7 @@ public class Persona {
         return nombre;
     }
 
-    void setNombre(String nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 }
